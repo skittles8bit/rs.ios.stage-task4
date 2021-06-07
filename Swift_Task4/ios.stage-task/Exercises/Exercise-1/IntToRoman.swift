c9710dfab6da31e64fa429d76aab79bbfc2333bd
Alexandr Karensky <skittles8bit@gmail.com>
2021-06-07 18:25:07 +0300

    init: implementation in to roman extension

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,32 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        
+        if (self < 1 || self > 3999) { return nil }
+        
+        var integerValue = self
+        var numeralString = ""
+        let mappingList = [(1000, "M"),
+                           (900, "CM"),
+                           (500, "D"),
+                           (400, "CD"),
+                           (100, "C"),
+                           (90, "XC"),
+                           (50, "L"),
+                           (40, "XL"),
+                           (10, "X"),
+                           (9, "IX"),
+                           (5, "V"),
+                           (4, "IV"),
+                           (1, "I")]
+        
+        for value in mappingList {
+            while integerValue >= value.0 {
+                integerValue -= value.0
+                numeralString += value.1
+            }
+        }
+        
+        return numeralString
     }
 }
